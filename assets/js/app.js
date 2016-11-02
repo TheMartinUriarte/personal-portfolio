4633ce68ef4dd71b94cb2cae9224f611c4a8ac0d
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2016-11-02 09:27:46 -0700

    Added dropdown button with HW Repos attached

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,30 +2,3 @@
 console.log("app.js linked.");
 
 // wait for DOM to load before running JS
-$(function() {
-
-    var source = $('#repo-template').html();
-    var template = Handlebars.compile(source);
-    var repoHtml;
-    var githubAPI = 'https://api.github.com/users/themartinuriarte/';
-    var repoURL = 'https://api.github.com/repos/themartinuriarte/';
-    var validRepos = [];
-
-
-
-    martinsRepos.forEach(function(repo) {
-        console.log("Ping!");
-        if (repo.description && repo.language) {
-            validRepos.push(repo);
-            //console.log(repo);
-        }
-    });
-    repoHtml = template({
-        repos: validRepos
-    });
-    $("#repo-list").append(repoHtml);
-
-});
-
-
-var martinsRepos = [{}];
